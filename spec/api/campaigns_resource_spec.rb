2508912260a914d197bed722c248c409f82f0af8
Bill Siggelkow <bsiggelkow@mac.com>
2014-08-14 10:53:02 -0400

    Add spec helper to parse json

--- spec/api/campaigns_resource_spec.rb
+++ spec/api/campaigns_resource_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "campaigns resource", :type => :request do
   it 'fetches the list of campaigns' do
     get '/campaigns.json'
-    expect(response).to be_success
+    expect(response.status).to eq(200)
+    expect(json).to eq([])
   end
 end
