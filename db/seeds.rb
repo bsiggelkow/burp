f2b38f1430f7fee80dac0db1eab551ecf2f7abf1
Bill Siggelkow <bill.siggelkow@blinqmedia.com>
2014-09-05 09:59:22 -0400

    Add seeds for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+client = Client.create(
+  name: 'Client 1', 
+  contact: 'foo@bar.com'
+)
+account = Account.create(
+  name: 'Account 1', 
+  client: client
+)
+fakebook = Platform.create(
+  name: 'Fakebook', 
+  base_url: 'http://graph.fakebook.com', 
+  features: '{"foo":"bar"}'
+)
+fwitter = Platform.create(
+  name: 'Fwitter', 
+  base_url: 'http://www.fwitter.com', 
+  features: '{"baz":"buz"}'
+)
+platform_account_type = PlatformAccountType.create(
+  name: 'Facebook Ad Account', 
+  platform: platform,
+  endpoint: 'http://graph.fakebook.com/accounts/',
+  schema: '{"title": "Example Schema", "type": "object", "properties": {"firstName": {"type": "string"}, "lastName": {"type": "string"}, "age": {"description": "Age in years", "type": "integer", "minimum": 0}}, "required": ["firstName", "lastName"]}',
+  field_mapping: '{}'
+)
