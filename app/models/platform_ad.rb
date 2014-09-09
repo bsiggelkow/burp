4693bde41af53934062b533192d09e7da3c7ba6a
Bill Siggelkow <bill.siggelkow@blinqmedia.com>
2014-09-09 13:36:08 -0400

    Incorporate transformer into models

--- app/models/platform_ad.rb
+++ app/models/platform_ad.rb
@@ -2,4 +2,12 @@ class PlatformAd < ActiveRecord::Base
   belongs_to :ad
   belongs_to :platform_ad_type
   validates :name, presence: true
+
+  after_initialize :defaults
+
+  private
+
+  def defaults
+    self.features ||= {}
+  end
 end
--- app/models/platform_ad_type.rb
+++ app/models/platform_ad_type.rb
@@ -2,4 +2,11 @@ class PlatformAdType < ActiveRecord::Base
   include PlatformTransformer
   belongs_to :platform
   validates :name, presence: true
+
+  def build_platform_ad(source)
+    target = PlatformAd.new(platform_ad_type: self)
+    transform(source,target)
+    target
+  end
+
 end
